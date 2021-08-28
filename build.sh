62ab7824ca11387ba2b1faf8d69355785b472b48
Martin Simon <me@martinsimon.me>
2021-08-28 16:57:54 +0200

    v7.1.0 - first version using topgrade-debian

---
+++ build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -eu
+
+IMAGE="debian:bullseye-slim"
+TARGET="$(dirname "$0" | xargs realpath)"
+VERSION="v7.1.0"
+
+while getopts "v:i:h" opt
+do
+    case "$opt" in
+        v)
+            VERSION="$OPTARG"
+            ;;
+        i)
+            IMAGE="$OPTARG"
+            ;;
+        h)
+            echo "Usage: $0 [-i image] [-v version]"
+            exit 0
+            ;;
+        *)
+            exit 1
+            ;;
+    esac
+done
+
+main() {
+    docker run --rm --name topgrade-build-$$ \
+                    --volume "$TARGET:/target" \
+                    --workdir /target \
+                    --env "VERSION=$VERSION" \
+                    --user root "$IMAGE" \
+                    sh entrypoint.sh
+}
+
+main
